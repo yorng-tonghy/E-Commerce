06d3f91d0c829045fa9f3ee08e71137c151cca87
tonghy <tonghy@>
2023-06-23 18:09:15 +0700

    fixing

---
+++ E-commerce_Frontend/src/main.js
@@ -0,0 +1,45 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/css/style.scss";
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount("#app");
+
+// import { createApp } from "vue";
+// import App from "/views/App.vue";
+// import router from "./router";
+
+
+// const app = createApp(App);
+
+// app.use(router);
+
+// app.mount("#app");
+
+//swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
